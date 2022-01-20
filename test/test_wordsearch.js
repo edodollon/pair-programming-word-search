eef293c1f0d879e1fbd3f45a9f0f80e300375bf1
Jomel Edodollon <jomeledodollon@yahoo.ca>
2022-01-19 23:31:41 -0800

    Pair programming solution, stretch incomplete

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,34 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  // it("should return true if the word is present diagonally", function() {
+  //   const result = wordSearch([
+  //     ['M', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  //     ['S', 'A', 'I', 'N', 'F', 'E', 'L', 'D'],
+  //     ['Y', 'F', 'R', 'F', 'Q', 'U', 'A', 'L'],
+  //     ['H', 'M', 'M', 'K', 'E', 'V', 'R', 'G'],
+  //     ['W', 'H', 'S', 'A', 'Y', 'E', 'R', 'L'],
+  //     ['B', 'F', 'R', 'E', 'Z', 'E', 'Y', 'B'],
+  //     ['U', 'B', 'K', 'W', 'A', 'K', 'A', 'I'],
+  //     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S']
+  //   ], 'MARK')
+
+  //   assert.isTrue(result);
+  // });
+
+  // it("should return true if the word is present diagonally", function() {
+  //   const result = wordSearch([
+  //     ['M', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  //     ['S', 'L', 'I', 'N', 'F', 'E', 'L', 'D'],
+  //     ['J', 'F', 'R', 'F', 'Q', 'U', 'A', 'L'],
+  //     ['H', 'O', 'M', 'K', 'E', 'V', 'R', 'G'],
+  //     ['W', 'H', 'M', 'A', 'Y', 'E', 'R', 'L'],
+  //     ['B', 'F', 'R', 'E', 'Z', 'E', 'Y', 'B'],
+  //     ['U', 'B', 'K', 'W', 'L', 'K', 'A', 'I'],
+  //     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S']
+  //   ], 'JOMEL')
+
+  //   assert.isTrue(result);
+  // });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,60 @@
+const transpose = function(matrix) {
+    let outputMatrix = [];
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (!outputMatrix[j]) {
+          outputMatrix.push([matrix[i][j]]);
+        } else {
+          outputMatrix[j].push(matrix[i][j]);
+        }
+      }
+    }
+    return outputMatrix;
+  };
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    for (const l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+    // let matrixForShifting = letters;
+    // for (let i = 1; i < matrixForShifting.length; i++) {
+    //     matrixForShifting[i].shift(i)
+    // }
+    // console.log(matrixForShifting);
+    // let diagonalJoin1 = [];
+    // let diagonalJoin2 = [];
+    // for (let i = 0; i < letters[0].length; i ++) {
+    //     for (let j = i; j >= 0; j --) {
+    //         diagonalJoin[i] += letters[i][j];
+    //     }
+    // }
+    // for (let i = 0; i < letters[0].length; i ++) {
+    //     for (let j = 1; j <= letters[i].length; j ++) {
+    //         diagonalJoin[i] += letters[i][j];
+    //     }
+    // }
+    // for (let i = 0; i < letters[0].length; i ++) {
+    //     diagonalJoin1[i] = letters[0][i];
+    //     for (let j = 1; j <= i; j ++) {
+    //         diagonalJoin1[i] += letters[j][i-j];
+    //     }
+    // }
+
+    // for (let i = letters[0].length - 1; i > 0; i --) {
+    //     diagonalJoin2[i] = letters[letters[0].length - 1][i];
+    //     for (let j = 1; j <= i; j ++) {
+    //         diagonalJoin2[i] += letters[letters.length-j][i-j];
+    //     }
+    // }
+    
+    // console.log('DIA1:', diagonalJoin1);
+    // console.log('DIA2:', diagonalJoin2);
+    return false;
 }
 
 module.exports = wordSearch
